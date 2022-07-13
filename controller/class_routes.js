4bc106db80dc500b43114101cf84eefe82f5f38f
Trevor Zou <tzou@olin.edu>
2022-07-13 10:14:07 -0500

    create new class without adding to database

--- controller/class_routes.js
+++ controller/class_routes.js
@@ -18,4 +18,36 @@ router.get('/', (req, res) =>{
         })
 })
 
+//=============================
+//GET Request Create Class
+//=============================
+router.get('/new', (req, res) =>{
+    // console.log(req.session)
+    // let session = req.session
+    res.render('classes/new')
+})
+
+//=============================
+//POST Request Create Class
+//=============================
+router.post('/', (req,res) =>{
+    console.log(req.body)
+    let copy = {...req.body}
+    let{name, place, cat1, cred1, cat2, cred2, fullfills} = copy
+    let newClass = {
+        name: name,
+        place: place,
+        credit_and_category: {
+            [cat1]: parseInt(cred1)
+        }
+    }
+    if(cat2 != ''){
+        newClass.credit_and_category[cat2] = parseInt(cred2)
+    }
+    console.log(newClass)
+    
+    res.redirect("/classes/new")
+})
+
+
 module.exports = router
