891d3c7280fd817a8252893a19e1b598d4677e10
Trevor Zou <tzou@olin.edu>
2022-07-15 14:49:00 -0500

    set up edit of class with mark modified

--- controller/schedule_routes.js
+++ controller/schedule_routes.js
@@ -4,11 +4,13 @@ const User = require('../models/user')
 const router = express.Router()
 const classSchema = Class.schema
 
-router.get('/', async function (req,res, next){
+router.get('/', (req,res) => {
+    console.log("got to schedule")
     let userId = req.session.userId
 
     User.findById(userId)
     .then( (usr)=>{
+        console.log("USR IN SCHEDULE", usr)
         let classList = usr.classes
         let classstruct = []
         for(let i=1;i<=8;i++){
@@ -71,7 +73,7 @@ router.delete("/:id/:semester", (req,res) =>{
 })
 
 //=============================
-//Edit a class semester
+//GET class semester
 //=============================
 router.get("/:id/:semester", (req,res) =>{
     const userId = req.session.userId
@@ -102,6 +104,57 @@ router.get("/:id/:semester", (req,res) =>{
         })
 
 })
+
+//=============================
+//PUT edit class semester
+//=============================
+router.put("/:id/:semester", (req,res) =>{
+    const userId = req.session.userId
+    const ID = req.params.id
+    console.log("ID AT TOP: ", ID)
+    const semnumb = parseInt(req.params.semester)
+    const semester_to_change_to = parseInt(req.body.semnumbtochangeto)
+    User.findById(userId)
+        .then(fuser =>{
+            console.log("SEMESTER_TO_CHANGE_TO: ",semester_to_change_to)
+            console.log("FUSER>CLASSES: )", fuser.classes)
+            const newmap = fuser.classes.map(ell =>{
+                // console.log("ell.class: ",ell.class,"ID: ",ID)
+                if((ell.class == ID) && (ell.semester == semnumb)){
+                    
+                    ell.semester = semester_to_change_to
+                    console.log("chnged semester")
+                    return ell
+                }
+                else{
+                    // console.log("iteratednotfound")
+                    return ell
+                }
+            })
+            console.log("NEWMAP: ", newmap)
+            fuser.classes = newmap
+            fuser.markModified('classes')
+            console.log("NEW FUSER>CLASSES: ", fuser.classes)
+            return fuser.save()
+                .then(savedDoc =>{
+                    console.log(fuser, savedDoc,"savedDoc === fuser ",savedDoc === fuser)
+                    res.redirect('/schedule')
+                })
+                
+            
+            
+
+        })
+        .catch(err=>{
+            console.log("error finding user for class edit: ", err)
+        })
+        
+
+})
+
+
+
+
 
 
 module.exports = router
